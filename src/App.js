f1976db37fca8bbd75b63d876901d6e3ce1096a4
ritika <ritika.juneja@byjus.com>
2024-04-27 15:40:16 +0530

    initial setup with react, parcel, lint and babel

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+
+  return (
+    <div className="app">
+      Hi ritika 
+    </div>
+  );
+}
+
+export default App;
